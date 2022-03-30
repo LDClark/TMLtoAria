549339cc0fa47700aaf7d711d08b2b1661f09f59
Landon Clark <landon.d.clark@gmail.com>
2022-03-30 07:38:19 -0600

    Update TMLReader.cs

--- TMLtoAria/TMLReader.cs
+++ TMLtoAria/TMLReader.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Windows;
 using VMS.TPS.Common.Model.API;
@@ -13,7 +14,7 @@ namespace TMLtoAria
 {
     public class TMLReader
     {
-        public static PdfDocument ConvertTMLtoPDF(string filePathTML)
+        public static PdfDocument ConvertTMLtoPDF(string filePathTML, PlanViewModel plan)
         {          
             List<string> TMLSheet = ParseTML(filePathTML);
 
@@ -22,7 +23,7 @@ namespace TMLtoAria
             int xPos = 0;
             int yPos = 0;
             int margin = 0;
-            int fontSize = 30/3;
+            int fontSize = 30;
             int fontWeight = 300;
             string fontFamily = "Arial";
             XFont font = new XFont(fontFamily, fontSize, GetFontStyle(fontWeight));
@@ -30,7 +31,9 @@ namespace TMLtoAria
             int lineSpacing = 1;
             int sectionHeight = 300;
             int i = 0;
+            string currentLine = string.Empty;
             string pageOrientation = "Portrait";
+            PdfPage currentPage = new PdfPage();
             PdfPage firstPage = new PdfPage();
 
             try
@@ -38,6 +41,7 @@ namespace TMLtoAria
                 foreach (string line in TMLSheet)
                 {
                     i++;
+                    currentLine = line;
                     if (string.IsNullOrWhiteSpace(line))
                         continue;
                     if (line.StartsWith("#"))
@@ -54,7 +58,7 @@ namespace TMLtoAria
                     if (line.StartsWith("Font"))
                     {
                         var value = line.Split('=')[1];
-                        //fontSize = Convert.ToInt32(value.Split(';')[0])/3;
+                        fontSize = Convert.ToInt32(value.Split(';')[0])/3;
                         fontWeight = Convert.ToInt32(value.Split(';')[1]);
                         fontFamily = value.Split(';')[2];
                         font = new XFont(fontFamily, fontSize, GetFontStyle(fontWeight));
@@ -160,14 +164,10 @@ namespace TMLtoAria
                         using (XGraphics gfx = XGraphics.FromPdfPage(firstPage))
                         {
                             XPen lineBlack = new XPen(XColors.Black, 5);
-                            gfx.DrawLine(lineBlack, xPos, yPos, xPos + firstPage.Width, yPos);
+                            gfx.DrawLine(lineBlack, xPos, yPos, xPos + currentPage.Width, yPos);
                         }
                     }
                     if (line.StartsWith("LineSpacing"))
-                    {
-
-                    }
-                    if (line.StartsWith("LineText"))
                     {
 
                     }
@@ -184,12 +184,12 @@ namespace TMLtoAria
                         var value = line.Split('=')[1];
                         if (line.Contains(";"))
                         {
-                            xPos = Convert.ToInt32(value.Split(';')[0]) + margin;
+                            xPos = Convert.ToInt32(value.Split(';')[0])/3 + margin/3;
                             yPos = Convert.ToInt32(value.Split(';')[1]);
                         }
                         else // move(0) or move(100)
                         {
-                            xPos = Convert.ToInt32(value) + margin;
+                            xPos = Convert.ToInt32(value)/3 + margin/3;
                             if (value == "0")
                             {
                                 yPos += 20;
@@ -198,14 +198,27 @@ namespace TMLtoAria
                     }
                     if (line.StartsWith("PageBreak"))
                     {
-                        if (pageOrientation == "Landscape")
+                        var value = line.Split('=')[1];
+                        if (value != "0.0")
                         {
-                            firstPage.Orientation = PdfSharp.PageOrientation.Landscape;
+                            yPos += 20;
+                            continue;
                         }
                         else
                         {
-                            firstPage.Orientation = PdfSharp.PageOrientation.Landscape;
+                            yPos = 0;
                         }
+                        PdfPage newPage = new PdfPage();
+                        if (pageOrientation == "Landscape")
+                        {
+                            newPage.Orientation = PdfSharp.PageOrientation.Landscape;
+                        }
+                        else
+                        {
+                            newPage.Orientation = PdfSharp.PageOrientation.Landscape;
+                        }                   
+                        outputDocument.Pages.Add(newPage);
+                        currentPage = newPage;
                     }
                     if (line.StartsWith("PaperOrientation"))
                     {
@@ -220,6 +233,7 @@ namespace TMLtoAria
                             pageOrientation = "Portrait";
                         }
                         outputDocument.Pages.Add(firstPage);
+                        currentPage = firstPage;
                     }
                     if (line.StartsWith("PaperSize"))
                     {
@@ -249,12 +263,12 @@ namespace TMLtoAria
                         var value = line.Split('=')[1];
                         if (line.Contains(";"))
                         {
-                            xPos += Convert.ToInt32(value.Split(';')[0]) + margin;
+                            xPos += Convert.ToInt32(value.Split(';')[0])/3 + margin/3;
                             yPos += Convert.ToInt32(value.Split(';')[1]);
                         }
                         else // move(0) or move(100)
                         {
-                            xPos += Convert.ToInt32(value) + margin;
+                            xPos += Convert.ToInt32(value)/3 + margin/3;
                             if (value == "0")
                             {
                                 yPos += 20;
@@ -274,7 +288,44 @@ namespace TMLtoAria
                     if (line.StartsWith("Text") || line.StartsWith("LineText"))
                     {
                         text = line.Split('=')[1];
-                        using (XGraphics gfx = XGraphics.FromPdfPage(firstPage))
+                        if (text.Contains("$"))
+                        {
+                            var value = text.Split('$')[1].Trim();
+                            if (value == "ImageId")
+                                text = plan.PlanImageId;
+                            if (value == "ImageUserOrigin")
+                                text = plan.ImageUserOrigin;
+                            if (value == "CourseId")
+                                text = plan.CourseId;
+                            if (value == "CourseIntent")
+                                text = plan.CourseIntent;
+                            if (value == "StructureSetId ")
+                                text = plan.PlanStructureSetId;
+                            if (value == "PatientTreatmentOrientation")
+                                text = plan.TreatmentOrientation;
+                            if (value == "PlanId")
+                                text = plan.PlanId;
+                            if (value == "PlanName")
+                                text = plan.PlanName;
+                            if (value == "PlanIntent")
+                                text = plan.PlanIntent;
+                            if (value == "PrimaryOncologistName")
+                                text = plan.PatientPrimaryOncologist;
+                            if (value == "PatientSex")
+                                text = plan.PatientSex;
+                            if (value == "PatientDateOfBirth")
+                                text = plan.PatientBirthdate.ToString();
+                            if (value == "PatientId")
+                                text = plan.PatientId;
+                            if (value.Contains("PatientLastName"))
+                                text = plan.PatientName;
+                            if (value.Contains("$TargetVolume"))
+                                text = plan.TargetVolumeId;
+                            if (value.Contains("PrimaryRefPointId"))
+                                text = plan.PrimaryReferencePointId;
+                        }
+
+                        using (XGraphics gfx = XGraphics.FromPdfPage(currentPage))
                         {
                             gfx.DrawString(text, font, XBrushes.Black,
                                            new XRect(xPos, yPos, 100, 10),
@@ -289,7 +340,7 @@ namespace TMLtoAria
             }
             catch (Exception)
             {
-                MessageBox.Show("Error reading TML file \n " + filePathTML + "\n" + "at line" + i);
+                MessageBox.Show("Error reading TML file \n " + filePathTML + "\n" + "at line " + (i + 1) + ": \n" + currentLine);
             }
 
             return outputDocument;
